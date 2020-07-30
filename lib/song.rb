aba218360f038dec843821d0f184520abaecbaaf
Raveen K Sharma <raveen08@gmail.com>
2020-07-30 19:10:18 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -25,11 +25,5 @@ class Song
     song
   end
  
-  def artist_name=(name)
-    if (self.artist.nil?)
-      self.artist = Artist.new(name)
-    else
-      self.artist.name = name
-    end
-  end
+  
 end
