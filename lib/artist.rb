460aafa47b950393dcf226e4c732fd2f3f66afff
Raveen K Sharma <raveen08@gmail.com>
2020-07-30 18:39:12 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -23,7 +23,12 @@ class Artist
   end
   
   def self.find_or_create_by_name(name)
-    
+    if (self.artist.nil?)
+      self.artist = Artist.new(name)
+    else
+      self.artist.name = name
+    end
+  end
     
   
   
